639af179b08308650b38ee11e6c30a438cfb6a3b
Markus Zabel <zabelm@admins-MacBook-Air-7.local>
2016-02-10 11:08:42 +0100

    100216

--- Markenabfrage.sql
+++ Markenabfrage.sql
@@ -5,7 +5,7 @@ on dd.[Dimension Value Code] = dv.[Code]
 Where dd.[Dimension Code] = 'MARKE' and dv.Code is NULL
 order by dd.[Dimension Value Code]
 
---Update urban_NAV600.dbo.[Urban-Brand GmbH$Document Dimension] set [Dimension Value Code] = 'BUBBLE KID BERLIN' Where [Dimension Value Code] = 'BUBBLE. KID BERLIN' and [Dimension Code] = 'MARKE'  
+--Update urban_NAV600.dbo.[Urban-Brand GmbH$Document Dimension] set [Dimension Value Code] = 'DçÖGENERATION' Where [Dimension Value Code] = 'D¥GENERATION' and [Dimension Code] = 'MARKE'  
 
 Select  distinct dd.[Dimension Value Code] as UsedCodesForItems
 From urban_NAV600.dbo.[Urban-Brand GmbH$Default Dimension]  as dd with (NOLOCK)
@@ -14,4 +14,4 @@ on dd.[Dimension Value Code] = dv.[Code]
 Where dd.[Dimension Code] = 'MARKE' and dv.Code is NULL
 order by dd.[Dimension Value Code]
 
---Update urban_NAV600.dbo.[Urban-Brand GmbH$Default Dimension] set [Dimension Value Code] = 'BUBBLE KID BERLIN' Where [Dimension Value Code] = 'BUBBLE. KID BERLIN' and [Dimension Code] = 'MARKE'  
+Update urban_NAV600.dbo.[Urban-Brand GmbH$Default Dimension] set [Dimension Value Code] = 'NESTLE' Where [Dimension Value Code] = 'NESTL…' and [Dimension Code] = 'MARKE'  
--- letzte 5 Auftragsübermittlungen zu Fiege mit Anzahl und Zeit.sql
+++ letzte 5 Auftragsübermittlungen zu Fiege mit Anzahl und Zeit.sql
@@ -1,13 +1,13 @@
-Select count(ffs.No_), ffs.[File No_], cast( pro.[Timestamp Upload_Download] as smalldatetime) as UploadZeit
+Select count(ffs.No_) as AnzahlAufträge, ffs.[File No_], cast( pro.[Timestamp Upload_Download] as smalldatetime) as UploadZeit
 From [urban_NAV600].[dbo].[Urban-Brand GmbH$eBayFFSalesHeader] as ffs with (NOLOCK)
 Left Join [urban_NAV600].[dbo].[Urban-Brand GmbH$eBayFFProtocol] as pro on ffs.[File No_] = pro.[File No_] 
 
 Where ffs.[File No_] in
 (
-Select TOP 15 [File No_]
+Select TOP 50 [File No_]
 From [urban_NAV600].[dbo].[Urban-Brand GmbH$eBayFFProtocol]
-Where [Setup Code] = 'WDB_MACHTL' and left([File No_],2) = 'A2' 
-Order by [File No_] desc)
+Where left([File No_],3) in (/*'A1',*/'A25'/*,'A6'*/)
+Order by [Timestamp Upload_Download] desc)
 
 Group by ffs.[File No_], pro.[Timestamp Upload_Download]
 order by pro.[Timestamp Upload_Download] desc
